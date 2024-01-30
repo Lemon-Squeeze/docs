bf86898e83c5d822948b9f704960b358c748306e
Thomas Ailleaume <thomasailleaume@Thomass-MacBook-Air.local>
2024-01-30 17:51:08 +0100

    add: price token

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const options = {method: 'GET', headers: {'X-API-KEY': 'a748e41815bc4a88b8276fd98d40eab6'}};
+
+    fetch('https://public-api.birdeye.so/public/price?address=4yt66dkPkrZYMhmAHQnM7gKjAGBTXn1YkMco998i8pSk', options)
+        .then(response => response.json())
+        .then(response => {
+            if(response.date) {
+                const price = response.data.value;
+                document.getElementById('tokenPrice').innerText = price.toFixed(8);
+            }else {
+                document.getElementById('tokenPrice').innerText = "---";
+            }
+        })
+        .catch(err => console.error(err));
+});
